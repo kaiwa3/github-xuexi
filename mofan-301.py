f776f88d3a7641f4485628f1793600819c53ba1d
addwang <addwang@gmail.com>
2018-06-28 18:17:14 +0800

    https://github.com/kaiwa3/github-xuexi.git

--- mofan-301.py
+++ mofan-301.py
@@ -11,44 +11,25 @@ numpy
 import tensorflow as tf
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
 
 tf.set_random_seed(1)
 np.random.seed(1)
 
-# # fake data
-# x = np.linspace(-1, 1, 100)[:, np.newaxis]          # shape (100, 1)
-# noise = np.random.normal(0, 0.1, size=x.shape)
-# y = np.power(x, 2) + noise                          # shape (100, 1) + some noise
-
-# plot data
-# plt.scatter(x, y)
-# plt.show()
-
-# 导入自己的数据测试
-f = open('aapl.csv')
-df = pd.read_csv(f)  #读入股票数据,df=pd.read_csv(f)
-x = np.array(df['Low'])   #获取最高价序列
-y = np.array(df['High'])
-
+# fake data
+x = np.linspace(-1, 1, 100)[:, np.newaxis]          # shape (100, 1)
+noise = np.random.normal(0, 0.1, size=x.shape)
+y = np.power(x, 2) + noise                          # shape (100, 1) + some noise
 
 # # plot data
 # plt.scatter(x, y)
 # plt.show()
-print('x','y')
-print(x,y)
 
 tf_x = tf.placeholder(tf.float32, x.shape)     # input x
 tf_y = tf.placeholder(tf.float32, y.shape)     # input y
 
-print('tf_x','tf_y')
-print(tf_x,tf_y)
-
 # neural network layers
 l1 = tf.layers.dense(tf_x, 10, tf.nn.relu)          # hidden layer
 output = tf.layers.dense(l1, 1)                     # output layer
-
-
 
 loss = tf.losses.mean_squared_error(tf_y, output)   # compute cost
 optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.5)
